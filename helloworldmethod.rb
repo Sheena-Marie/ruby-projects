8b1e92cf15ff9c342347aca14a8abcca6c0e3163
Sheena Takkenberg <sheena.takkenberg@gmail.com>
2016-08-31 15:35:08 +1000

    adding files

---
+++ helloworldmethod.rb
@@ -0,0 +1,7 @@
+def hello(anything)
+  puts anything
+end
+
+hello "My name is Sheena"
+hello "Welcome to the rails developer course"
+hello "Check it out, this prints anything I want"
---
+++ helloworldstring.rb
@@ -0,0 +1,5 @@
+ hello = "Hello World!"
+ puts hello
+
+ hello = "Welcome to the complete ruby on rails developer course"
+ puts hello
